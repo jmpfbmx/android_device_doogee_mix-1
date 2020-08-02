328633b089250de1de1c915ba6e6c3c6d665d128
jmpfbmx <jmpf.bmx@gmail.com>
2020-08-02 23:34:50 +0200

    mix: Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -14,26 +14,34 @@
 # limitations under the License.
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := mix
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/doogee/mix/device_mix.mk)
-# $(call inherit-product, device/doogee/mix/prop.mk)
 
+# BootAnimation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Release name
+PRODUCT_RELEASE_NAME := mix
+
 # Device identifier. This must come after all inclusions
+BOARD_VENDOR := DOOGEE
 PRODUCT_DEVICE := mix
 PRODUCT_NAME := lineage_mix
 PRODUCT_BRAND := DOOGEE
 PRODUCT_MODEL := DOOGEE MIX
 PRODUCT_MANUFACTURER := DOOGEE
+TARGET_VENDOR := DOOGEE
 
 # Google client ID property.
 PRODUCT_GMS_CLIENTID_BASE := android-DOOGEE
 
+# Use the latest approved GMS identifiers unless running a signed build && Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=DOOGEE_MIX \
+    PRODUCT_NAME=DOOGEE_MIX \
+    PRIVATE_BUILD_DESC="full_hct6757_66_n-user 7.0 NRD90M 1517376035 release-keys" \
+    BUILD_FINGERPRINT := DOOGEE/MIX/MIX:7.0/NRD90M/1513355947:user/release-keys
